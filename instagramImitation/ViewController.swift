59a5f114b35e5e26182cb774aa8235edea056741
Betty Pan <betty19931218@gmail.com>
2021-04-20 13:32:06 +0800

    Initial Commit

---
+++ instagramImitation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagramImitation
+//
+//  Created by Betty Pan on 2021/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
